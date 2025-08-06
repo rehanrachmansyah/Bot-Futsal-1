3c2b73a9056b8a1f4d8a9716a05c3dbd784fca0a
rehanrachmansyah <153190504+rehanrachmansyah@users.noreply.github.com>
2025-08-06 19:22:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,5 +82,8 @@ def send_message(to, message):
     print("RESPON ULTRAMSG:", response.text)
 
 if __name__ == '__main__':
+    print("✅ Aplikasi Flask berhasil dijalankan...")
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
